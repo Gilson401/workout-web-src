a5cba0931b1bc45d890376ae5856e36d6ce5ea77
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2023-01-14 07:06:02 -0300

    Untrack files in .gitignore

---
+++ lib/clockTime_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ClockTimerPage extends StatelessWidget {
+  const ClockTimerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('ClockTimerPage'),
+    );
+  }
+}
