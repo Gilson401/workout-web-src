e181300977bd014f25e65d97864a6bb211743ac0
Gilson.Conceicao <gilson.conceicao@al.infnet.edu.br>
2023-04-15 11:17:59 -0300

    feature: force portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/di/inject.dart';
 import 'widgets/myapp.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+    SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   startModules();
   runApp(const MyApp());
 }
